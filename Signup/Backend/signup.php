c626a176bb0bdb430b97e6b7e1f38693d425b176
Erdona Kadriolli <95415767+erdonakadriolli@users.noreply.github.com>
2025-02-11 15:35:42 +0100

    Add files via upload

---
+++ Signup/Backend/signup.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "focus";
+
+// Krijo lidhjen
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Kontrollo lidhjen
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (name, email, password) VALUES ('$name', '$email', '$password')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+$conn->close();
+?>
