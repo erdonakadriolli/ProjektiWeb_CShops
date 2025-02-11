b605c65755565a94b6c9599070726e91e18f466d
Erdona Kadriolli <donakadriolli@gmail.com>
2025-02-11 15:34:31 +0100

    .

--- SignUP/users.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-    id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    role ENUM('admin', 'user') NOT NULL
-);
